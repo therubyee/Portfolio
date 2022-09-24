3cfe8c869e4859186100f2a6f9fbfd4dfd7c5a98
RodDotO <rodrigooliveira887@gmail.com>
2022-09-24 14:16:47 -0300

    Futuro portifolio

---
+++ Js/main.js
@@ -0,0 +1,7 @@
+var cliqueAqui = document.querySelector(".btn");
+cliqueAqui.addEventListener('click', LinkedIn);
+
+function LinkedIn(){
+    window.open('https://www.linkedin.com/in/ro-tavares/');
+}
+
