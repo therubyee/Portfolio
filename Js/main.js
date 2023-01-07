92b7a71ab6104f76686e03dea470463739371c8c
RodDotO <rodrigooliveira887@gmail.com>
2023-01-07 02:57:59 -0300

    Update

--- Js/main.js
+++
@@ -1,7 +0,0 @@
-var cliqueAqui = document.querySelector("#linkedin");
-cliqueAqui.addEventListener('click', LinkedIn);
-
-function LinkedIn(){
-    window.open('https://www.linkedin.com/in/ro-tavares/');
-}
-
